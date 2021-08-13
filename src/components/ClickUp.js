ddd6ff1ca2cd926bf8b10ff88bc588b4dc72c7aa
JavierBec <javier.becerra@rokketlabs.com>
2021-08-13 15:49:08 -0400

    feat: implementing clickup

---
+++ src/components/ClickUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const ClickUp = () => {
+    return (
+        <iframe 
+            src="https://sharing.clickup.com/l/h/6-9460078-1/566388821f40a4e" 
+            className="clickup-embed" 
+            width="100%" height="700px" style={{'background': 'transparent', 'border': '1px solid #ccc'}}>
+        </iframe>
+    )
+}
